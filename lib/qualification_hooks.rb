01ca0cec2b3e479de31de299f1f099c152f4cb94
Veeraraghavan Sekar <veeraraghavan.sekar@alterway.fr>
2020-12-14 14:42:23 +0100

    Changed formatting as paragraph

--- lib/qualification_hooks.rb
+++ lib/qualification_hooks.rb
@@ -40,12 +40,12 @@ class QualificationHooks < Redmine::Hook::ViewListener
                 if(response["closest"])
                     updated_response=JSON.parse(response)
                     notes = "<p>"+ "<br>"+"-- Redmine Advise -- "+"<br>"
-                    notes << " Ticket le plus ressemblant: "+"<br>"
-                    notes << "id | autheur | commun% | date | titre "+"<br>"
-                    notes << adviseDetails(updated_response["closest"])
-                    notes << " Tickets les proches du même projet:"+"<br>"
-                    notes << "id | autheur | commun% | date | titre "+"<br>"
-                    notes << (updated_response["project_closests"].map {|x| adviseDetails(x)}).join("")+"</p>"
+                    notes + " Ticket le plus ressemblant: "+"<br>"
+                    notes + "id | autheur | commun% | date | titre "+"<br>"
+                    notes + adviseDetails(updated_response["closest"])
+                    notes + " Tickets les proches du même projet:"+"<br>"
+                    notes + "id | autheur | commun% | date | titre "+"<br>"
+                    notes + (updated_response["project_closests"].map {|x| adviseDetails(x)}).join("")+"</p>"
                     custom_field_values[service["target_field"]] = notes
                     
                 end
